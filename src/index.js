48918ab51702bda34e4be5af869c340eaa37078d
Miriam <miriamjoshua00@gmail.com>
2022-12-25 13:47:49 -0500

    commit tree fc12419898ad1e25c588e4e3e734ea7bee2db2d5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+
 }
 
 export function candyReducer(state = [], action) {
@@ -21,6 +36,8 @@ function render() {
     throw new Error("the store's state has not been defined yet");
   }
 }
+const store = createStore(candyReducer)
+store.dispatch({ type: "candies/add"})
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
